db3c6ebea16ecd1d5bec848c63badc9929a03259
SantiagoAlbea <santiialbea123@gmail.com>
2022-08-29 15:42:07 -0300

    Agrega dependencias para navegacion y pantallas de prueba de la misma

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { supabase } from './supabase';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button 
+        title="Ir a Detalles"
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Image source={require('./logo.jpeg')} style={{width: 400, height: 400}}/>
+    </View>
+  );
+}
+
+function DetailsScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button 
+        title="Ir al Inicio"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title:'Inicio' }}/>
+        <Stack.Screen name="Details" component={DetailsScreen} options={{ title:'Detalles' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
