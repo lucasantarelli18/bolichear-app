946ba6d2b25dafda8a9fb672d3d5f233a32a0478
makirodriguez <maaaki.rodriguez@gmail.com>
2022-09-02 19:54:43 -0300

    Agrego parte de la interfaz del alta de promociones

--- Screens/PromocionesScreen.js
+++ Screens/PromocionesScreen.js
@@ -1,59 +1,83 @@
-import React, { useState } from 'react';
-import { Text, Button, View, ScrollView, StyleSheet, Alert, TextInput} from 'react-native';
+import React, { useState } from "react";
+import { Button, View, Text, StyleSheet} from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import * as Backend from '../backlog';
+import Moment from 'moment';
+import 'moment-timezone';
 import 'react-native-url-polyfill/auto';
-
+import { Input, Block} from "galio-framework";
+import { TextInput } from "react-native-web";
 
 
 export function PromocionesScreen({navigation}) {
 
-    const [promociones, setPromociones] = React.useState([]);
-    const [password, setPassword] = React.useState('');
-    const [date, setDate] = useState(new Date())
-    const [open, setOpen] = useState(false)
+  const [promociones, setPromociones] = React.useState([]);
+ const [fechaYHoraIncio, setFechaYHoraIncio] = useState(false);
+ const [fechaYHoraFin, setfechaYHoraFin] = useState(false);
+
+ 
+ React.useEffect(() => {
+  //Tomo provincias
+Backend.insertPromocion()
+.then((items) => {
+console.log(items)
+setPromociones(items)
+})
+}, [])
+
+  const showDateTimePicker = () => {
+    setFechaYHoraIncio(true);
+  };
 
 
-    React.useEffect(() => {
-        //Tomo provincias
-    Backend.insertPromocion()
-    .then((items) => {
-    console.log(items)
-    setPromociones(items)
-    })
-    }, [])
+  const hideDateTimePicker = () => {
+    setFechaYHoraIncio(false);
+  };
 
-    console.log(promociones)
+  const handleConfirm = (fechaYHoraIncio) => {
+    console.log("A date has been picked: ", fechaYHoraIncio);
+    hideDateTimePicker();
+  };
+  
+ 
+  return (
+    <View style ={styles.container2}>
+      <Input placeholder="Nombre"></Input>
+      <Input placeholder="Inserte la descripcion"></Input>
+      <Block alignSelf='center'>
+      <Button style = {styles.input} title="Fecha y hora de inicio" onPress={showDateTimePicker} />
+      <DateTimePickerModal
+        isVisible={fechaYHoraIncio}
+        mode="datetime"
+        onConfirm={handleConfirm}
+        onCancel={hideDateTimePicker}
+      />
+      </Block>
+      <Block alignSelf='center'>
+      <Button style = {styles.input} title="Fecha y hora de fin" onPress={showDateTimePicker} />
+      <DateTimePickerModal
+        isVisible={fechaYHoraFin}
+        mode="datetime"
+        onConfirm={handleConfirm}
+        onCancel={hideDateTimePicker}
+      />
+      </Block>     
 
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Promociones</Text>
-          <TextInput
-          style = {styles.input}
-          placeholder="Nombre"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-          />
-         
       
-          <TextInput
-          style = {styles.input}
-          placeholder="Descripcion"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-          />
-        <Button style = {styles.input} title="Subir"  />
-        </View>
-      );
-}
+    </View>
+  );
+
+};
 
 const styles = StyleSheet.create({
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
-    padding: 10,
+    padding: 50,
   },
+  button:{
+    height:40,
+    width:30
+  }
 });
-
