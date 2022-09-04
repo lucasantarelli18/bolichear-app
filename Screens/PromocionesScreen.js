147e7c2b712e0ad7e691150438d351563884f82d
Macarena Rodriguez <maaaki.rodriguez@gmail.com>
2022-09-04 20:44:33 -0300

    Agrego opcion de cargar imagen

--- Screens/PromocionesScreen.js
+++ Screens/PromocionesScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, View, Text, StyleSheet, Pressable} from "react-native";
+import { Button, View, Text, StyleSheet, Pressable, Image,Modal} from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import * as Backend from '../backlog';
 import Moment from 'moment';
@@ -7,6 +7,8 @@ import 'moment-timezone';
 import 'react-native-url-polyfill/auto';
 import { Input, Block} from "galio-framework";
 import { TextInput } from "react-native-web";
+import * as ImagePicker from 'expo-image-picker';
+import {  Card } from 'react-native-elements';
 
 
 export function PromocionesScreen({navigation}) {
@@ -19,6 +21,8 @@ export function PromocionesScreen({navigation}) {
     idLocal: 'idLocal'});
   const [fechaYHoraIncio, setFechaYHoraIncio] = useState(false);
   const [fechaYHoraFin, setfechaYHoraFin] = useState(false);
+  const [image, setImage] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
 
  
   React.useEffect(() => {
@@ -62,33 +66,55 @@ export function PromocionesScreen({navigation}) {
     })
     console.log(promociones)
   }
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
   return (
     <View style ={styles.container}>
-      <Input style={styles.INPUT} placeholder="Nombre" name="nombre"onChange={handleConfirm}></Input>
-      <Input style={styles.INPUT} placeholder="Inserte la descripcion" name="descripcion" onChange={handleConfirm}></Input>
-      <Block alignSelf='center'>
-      <Button style = {styles.input} title="Fecha y hora de inicio" onPress={showDateTimePicker} />
-      <DateTimePickerModal
-        isVisible={fechaYHoraIncio}
-        mode="datetime"
-        onConfirm={handleConfirm}
-        onCancel={hideDateTimePicker}
-      />
-      </Block>
-      <Block alignSelf='center'>
-      <Button style = {styles.input} title="Fecha y hora de fin" onPress={showDateTimePicker} />
-      <DateTimePickerModal
-        isVisible={fechaYHoraFin}
-        mode="datetime"
-        onConfirm={handleConfirm}
-        onCancel={hideDateTimePicker}
-      />
-      </Block>     
+      <View style = {styles.container2}>
+        <Card>
+          <Input style={styles.INPUT} placeholder="Nombre" name="nombre"onChange={handleConfirm}></Input>
+          <Input style={styles.INPUT} placeholder="Inserte la descripcion" name="descripcion" onChange={handleConfirm}></Input>
+          <Block alignSelf='center'>
+          <Button style = {styles.input} title="Fecha y hora de inicio" onPress={showDateTimePicker} />
+          <DateTimePickerModal
+            isVisible={fechaYHoraIncio}
+            mode="datetime"
+            onConfirm={handleConfirm}
+            onCancel={hideDateTimePicker}
+          />
+          </Block>
+          <Block alignSelf='center'>
+          <Button style = {styles.input} title="Fecha y hora de fin" onPress={showDateTimePicker} />
+          <DateTimePickerModal
+            isVisible={fechaYHoraFin}
+            mode="datetime"
+            onConfirm={handleConfirm}
+            onCancel={hideDateTimePicker}
+          />
+          </Block>     
+          <Button title="Cargar imagen" onPress={pickImage} />
+          {image && <Image source={{ uri: image }} style={{ width: 350, height: 200 }} />}
 
-      <Pressable style={styles.button} onPress={nuevaPromo}>
-          <Text style={styles.text}>Cargar</Text>
-        </Pressable>
+          <Pressable style={styles.button} onPress={nuevaPromo}>
+              <Text style={styles.text}>Cargar</Text>
+          </Pressable>
+        </Card>
 
+      </View>
       
     </View>
   );
@@ -97,15 +123,44 @@ export function PromocionesScreen({navigation}) {
 
 const styles = StyleSheet.create({
   INPUT:{
-    INPUT_BORDER_RADIUS:	16 * 0.5,
+    INPUT_BORDER_RADIUS: 16 * 0.5,
     INPUT_BORDER_WIDTH:	16 * 0.05,
     INPUT_HEIGHT:	16 * 2.75,
     INPUT_HORIZONTAL:	16,
     INPUT_TEXT:	16 * 0.875,
     INPUT_LABEL_TEXT:	16 * 0.9,
     INPUT_LABEL_BOTTOM:	16 / 4,
-    INPUT_HELP_TEXT:	16 * 0.8,
+    INPUT_HELP_TEXT: 16 * 0.8,
     INPUT_ROUNDED:	16 * 1.7
-  }
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent: "space-between",
+  },
+  container2: {
+    width: '90%',
+    height: '100%',
+    backgroundColor: '#000',
+    paddingVertical: 110
+  },
 
 });
