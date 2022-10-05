ab15714143863012cad47a09f58c28801e8c717a
Macarena Rodriguez <maaaki.rodriguez@gmail.com>
2022-10-05 11:59:24 -0300

    Agrego env

--- supabase.js
+++ supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 import { AsyncStorage } from '@react-native-async-storage/async-storage';
 
-//console.log(process.env.SUPABASE_URL);
+console.log(process.env.SUPABASE_URL);
 //console.log(process.env.SUPABASE_KEY);
 
-export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY, {
+export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY,  {
     localStorage: AsyncStorage,
 });
